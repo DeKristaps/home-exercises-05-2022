e114c8f1274c1be62f80eb4aa30e44273b9df803
DeKristaps <dekristaps@gmail.com>
2022-09-15 10:26:08 +0300

    Labojumi pirmaja md

--- Homework/src/io/codelex/arithmetic/practice/CozaLozaWoza.java
+++ Homework/src/io/codelex/arithmetic/practice/CozaLozaWoza.java
@@ -2,38 +2,26 @@ package io.codelex.arithmetic.practice;
 
 public class CozaLozaWoza {
     public static void main(String[] args) {
-        String printStatement;
-
-
         for (int i = 1; i <= 110; i++) {
-
-            if ((i % 7 == 0) && (i % 5 == 0)) {
-                printStatement = ("LozaWoza ");
-
-            } else if ((i % 7 == 0) && (i % 3 == 0)) {
-                printStatement = ("CozaWoza ");
-
-            } else if ((i % 5 == 0) && (i % 3 == 0)) {
-                printStatement = ("CozaLoza ");
-
-            } else if (i % 3 == 0) {
-                printStatement = ("Coza ");
-
-            } else if (i % 5 == 0) {
-                printStatement = ("Loza ");
-
-            } else if (i % 7 == 0) {
-                printStatement = ("Woza ");
-
-            } else {
-                printStatement = (i + " ");
+            StringBuilder outputString = new StringBuilder(" ");
+            if (i % 3 == 0) {
+                outputString.append("Coza");
             }
-
+            if (i % 5 == 0) {
+                outputString.append("Loza");
+            }
+            if (i % 7 == 0) {
+                outputString.append("Woza");
+            }
+            if (outputString.toString().equals(" ")) {
+                outputString.append(i);
+            }
             if (i % 11 == 0) {
-                System.out.println(printStatement);
-            } else {
-                System.out.print(printStatement);
+                outputString.append("\n");
             }
+            System.out.print(outputString);
         }
+
     }
 }
+
