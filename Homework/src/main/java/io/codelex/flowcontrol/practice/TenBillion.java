638d3f38055b91f3f736dc5ccb5e7f8f63d64e26
DeKristaps <dekristaps@gmail.com>
2022-09-23 23:23:39 +0300

    pedejais labojums

--- Homework/src/main/java/io/codelex/flowcontrol/practice/TenBillion.java
+++ Homework/src/main/java/io/codelex/flowcontrol/practice/TenBillion.java
@@ -22,32 +22,39 @@ public class TenBillion {
             if (n > 10000000000L) {
                 System.out.println("Number is greater or equals 10,000,000,000!");
             } else {
-                int digits = 1;
-                if ((n >= 10) && (n < 100)) {
-                    digits = 2;
-                } else if (n < 1000) {
-                    digits = 3;
-                } else if (n < 10000) {
-                    digits = 4;
-                } else if (n < 100000) {
-                    digits = 5;
-                } else if (n < 1000000) {
-                    digits = 6;
-                } else if (n < 10000000) {
-                    digits = 7;
-                } else if (n < 100000000) {
-                    digits = 8;
-                } else if (n < 1000000000) {
-                    digits = 9;
-                } else  {
-                    digits = 10;
-                }
+                int digits = swithcStatement(n);
+
                 System.out.println("Number of digits in the number: " + digits);
             }
         } else {
             System.out.println("The number is not a long");
         }
 
+    }
+
+    public static int swithcStatement(long n){
+        int digits = 1;
+        if ((n >= 10) && (n < 100)) {
+            digits = 2;
+        } else if (n < 1000) {
+            digits = 3;
+        } else if (n < 10000) {
+            digits = 4;
+        } else if (n < 100000) {
+            digits = 5;
+        } else if (n < 1000000) {
+            digits = 6;
+        } else if (n < 10000000) {
+            digits = 7;
+        } else if (n < 100000000) {
+            digits = 8;
+        } else if (n < 1000000000) {
+            digits = 9;
+        } else  {
+            digits = 10;
+        }
+
+        return digits;
     }
 
 }
